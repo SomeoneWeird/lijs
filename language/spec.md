b6d2cd55d22c1e8a399f073d13a996298bdef2e4
Adam Brady <adam@boxxen.org>
2016-06-06 18:09:51 +1000

    .

---
+++ language/spec.md
@@ -0,0 +1,42 @@
+## Variable Assignment
+
+```
+$ a 1
+```
+
+## Function Calling
+
+```
+(add one two)
+// add(one, two)
+```
+
+## Function declaration
+
+```
+def myFn [ one two ] {
+  (add one two)
+}
+
+## Line breaks
+
+Used for distinguishing expressions
+
+Invalid:
+
+```
+def myFn [ one two ] {
+  (add one two)
+}
+(add 1 2)
+```
+
+Valid:
+
+```
+def myFn [ one two ] {
+  (add one two)
+}
+
+(add 1 2)
+```
---
+++ test/tokenizer.js
@@ -0,0 +1,150 @@
+var assert = require('assert')
+
+var tokenizer = require('../lib/tokenizer')
+
+describe('tokenizer', function () {
+
+  describe('Whitespace', function () {
+    it('should skip whitespace', function () {
+      assert.equal(tokenizer('    ').length, 0)
+    })
+  })
+
+  describe('LineBreak', function () {
+    it('should parse properly', function () {
+      assert.deepEqual(tokenizer('\n\n'), [ {
+        type: 'LineBreak'
+      }, {
+        type: 'LineBreak'
+      } ])
+    })
+  })
+
+  describe('Literals', function () {
+
+    describe('Literal', function () {
+      it('should parse literal', function () {
+        var code = 'a b c'
+        var tokens = tokenizer(code)
+
+        assert.deepEqual(tokens, [ {
+          type: 'Literal',
+          value: 'a'
+        }, {
+          type: 'Literal',
+          value: 'b'
+        }, {
+          type: 'Literal',
+          value: 'c'
+        } ])
+      })
+    })
+
+    describe('NumberLiteral', function () {
+      it('should parse NumberLiteral', function () {
+        var code = '1 2 3 4'
+        var tokens = tokenizer(code)
+
+        assert.deepEqual(tokens, [ {
+          type: 'NumberLiteral',
+          value: '1'
+        }, {
+          type: 'NumberLiteral',
+          value: '2'
+        }, {
+          type: 'NumberLiteral',
+          value: '3'
+        }, {
+          type: 'NumberLiteral',
+          value: '4'
+        } ])
+      })
+    })
+
+    describe('StringLiteral', function () {
+      it('should parse StringLiteral', function () {
+        var code = "'hello'"
+        var tokens = tokenizer(code)
+
+        assert.deepEqual(tokens, [ {
+          type: 'StringLiteral',
+          value: 'hello'
+        } ])
+      })
+
+      it('should parse multiple StringLiteral', function () {
+        var code = "'hello   ' 'world''howareyou'"
+        var tokens = tokenizer(code)
+        assert.deepEqual(tokens, [ {
+          type: 'StringLiteral',
+          value: 'hello   '
+        }, {
+          type: 'StringLiteral',
+          value: 'world'
+        }, {
+          type: 'StringLiteral',
+          value: 'howareyou'
+        } ])
+      })
+    })
+  })
+
+  describe('should be able to mix literal types', function () {
+    it('Literal + NumberLiteral', function () {
+      var code = 'hello 137 world 42'
+      var tokens = tokenizer(code)
+
+      assert.deepEqual(tokens, [ {
+        type: 'Literal',
+        value: 'hello'
+      }, {
+        type: 'NumberLiteral',
+        value: '137'
+      }, {
+        type: 'Literal',
+        value: 'world'
+      }, {
+        type: 'NumberLiteral',
+        value: '42'
+      } ])
+    })
+
+    it('Literal + StringLiteral', function () {
+      var code = "hello 'hello' world 'world'"
+      var tokens = tokenizer(code)
+
+      assert.deepEqual(tokens, [ {
+        type: 'Literal',
+        value: 'hello'
+      }, {
+        type: 'StringLiteral',
+        value: 'hello'
+      }, {
+        type: 'Literal',
+        value: 'world'
+      }, {
+        type: 'StringLiteral',
+        value: 'world'
+      } ])
+    })
+
+    it('StringLiteral + NumberLiteral', function () {
+      var code = "'hello' 42 'world' 137"
+      var tokens = tokenizer(code)
+
+      assert.deepEqual(tokens, [ {
+        type: 'StringLiteral',
+        value: 'hello'
+      }, {
+        type: 'NumberLiteral',
+        value: '42'
+      }, {
+        type: 'StringLiteral',
+        value: 'world'
+      }, {
+        type: 'NumberLiteral',
+        value: '137'
+      } ])
+    })
+  })
+})
