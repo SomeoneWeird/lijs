7abea92dc0c1676216f1e8b0da8668bf487601b5
Adam Brady <adam@boxxen.org>
2016-06-10 21:12:54 +1000

    Add more to AST

--- test/2-ast.js
+++ test/2-ast.js
@@ -72,4 +72,84 @@ describe('AST', function () {
       } ])
     })
   })
+  describe('Literals', function () {
+    describe('Literal', function () {
+      it('should generate AST for Literal', function () {
+        testAST('hello', [ {
+          type: 'Literal',
+          value: 'hello'
+        } ])
+      })
+    })
+    describe('NumberLiteral', function () {
+      it('should generate AST for NumberLiteral', function () {
+        testAST('5', [ {
+          type: 'NumberLiteral',
+          value: '5'
+        } ])
+      })
+    })
+    describe('FloatLiteral', function () {
+      it('should generate AST for FloatLiteral', function () {
+        testAST('5.3', [ {
+          type: 'FloatLiteral',
+          value: '5.3'
+        } ])
+      })
+    })
+    describe('StringLiteral', function () {
+      it('should generate AST for StringLiteral', function () {
+        testAST("'hello'", [ {
+          type: 'StringLiteral',
+          value: 'hello'
+        } ])
+      })
+    })
+  })
+  describe('LineBreak', function () {
+    it('should ignore LineBreak', function () {
+      testAST('\n\n\n', [])
+    })
+  })
+  describe('Assignment', function () {
+    it('should error if variable name is invalid', function () {
+      assert.throws(function () {
+        testAST('$ 3 1')
+      }, /Variable name must be a literal/)
+    })
+    it('should generate AST for Number Assignment', function () {
+      testAST('$ a 1', [ {
+        type: 'Assignment',
+        name: 'a',
+        value: {
+          type: 'NumberLiteral',
+          value: '1'
+        }
+      } ])
+    })
+    it('should generate AST for Array Assignment', function () {
+      testAST('$ a [ b c d ]', [ {
+        type: 'Assignment',
+        name: 'a',
+        value: {
+          type: 'Array',
+          elements: [ {
+            type: 'Literal',
+            value: 'b'
+          }, {
+            type: 'Literal',
+            value: 'c'
+          }, {
+            type: 'Literal',
+            value: 'd'
+          } ]
+        }
+      } ])
+    })
+    it('should error if value is another Assignment', function () {
+      assert.throws(function () {
+        testAST('$ a $ b 1')
+      }, /Variable value cannot be assignment/)
+    })
+  })
 })
